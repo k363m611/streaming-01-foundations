b6687d60de13219ec2d7aa786e9447641c3b1cfb
k363m611 <k363m611@gmail.com>
2026-05-17 19:55:15 -0500

    Update producer_mcgaughey.py

--- src/streaming/producer_mcgaughey.py
+++ src/streaming/producer_mcgaughey.py
@@ -9,8 +9,8 @@ Work up to see how it all fits together.
 Many functions are standard helpers
 and should not need project-specific modifications.
 
-Author: Denise mcgaughey
-Date: 2026-05
+Author: katie mcgaughey
+Date: 2026-05 
 
 Terminal command to run this file from the root project folder:
 
