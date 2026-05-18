640aab620b4d0d7372b1d1d1a9c89fc4676e6f38
k363m611 <k363m611@gmail.com>
2026-05-17 19:52:32 -0500

    Update and rename producer_case.py to producer_mcgaughey.py

--- src/streaming/producer_mcgaughey.py
+++ src/streaming/producer_mcgaughey.py
@@ -1,4 +1,4 @@
-"""src/streaming/producer_case.py - Local producer example.
+"""src/streaming/producer_mcgaughey.py - Local producer example.
 
 Reads sales from data/sales.csv
 and writes records to a local simulated topic file one message at a time.
@@ -9,12 +9,12 @@ Work up to see how it all fits together.
 Many functions are standard helpers
 and should not need project-specific modifications.
 
-Author: Denise Case
+Author: Denise mcgaughey
 Date: 2026-05
 
 Terminal command to run this file from the root project folder:
 
-    uv run python -m streaming.producer_case
+    uv run python -m streaming.producer_mcgaughey
 
 OBS:
   Don't edit this file - it should remain a working example.
